0c723c29dc3a9c5c86c9a01e3cd267d4b496e4d9
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2024-05-22 21:09:16 +0100

    Fix export to default

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 import VueScrollShadow from "./components/VueScrollShadow.vue";
 
-export default {
-  install: (app: any) => {
-    app.component("vue-scroll-shadow", VueScrollShadow);
-  },
-};
+export default VueScrollShadow;
